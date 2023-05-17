18b4b57f6ea7a7757019f2929bb176c90e7ced3d
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-05-17 12:23:46 +0530

    fibanacci series

--- script1.js
+++ script1.js
@@ -1,8 +1,21 @@
 // 1-Palindrome number
 
-const isPalindrome = function (x) {
-  return x < 0 ? false : x == x.toString().split("").reverse().join("");
-  console.log();
+// const isPalindrome = function (x) {
+//   return x < 0 ? false : x == x.toString().split("").reverse().join("");
+//   console.log();
+// };
+// const res = isPalindrome(121);
+// console.log(res);
+
+// 2. fibonacci number
+
+const fib = function (n) {
+  const arr = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+  // console.warn(arr);
+  return console.warn(arr);
 };
-const res = isPalindrome(121);
-console.log(res);
+fib(5);
